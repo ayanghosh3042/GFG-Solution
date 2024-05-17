b30a96237374c8cf93eefacbdb491e97c60f594d
Ayan Ghosh <146657282+ayanghosh3042@users.noreply.github.com>
2024-05-17 13:16:12 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Selection Sort/selection-sort.java
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+import java.util.*;
+
+class GFG
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int t = sc.nextInt();
+		while(t>0)
+		{
+			int n = sc.nextInt();
+			int arr[] = new int[n]; 
+			for(int i=0;i<n;i++)
+			{
+				arr[i] = sc.nextInt();
+			}
+		
+			Solution obj = new Solution();
+			obj.selectionSort(arr, n);
+			
+			for(int i=0;i<n;i++)
+		    	System.out.print(arr[i]+" ");
+		    System.out.println();
+			t--;
+		}
+		
+	}
+}
+
+// } Driver Code Ends
+
+
+class Solution
+{
+	int  select(int arr[], int i)
+	{
+        return arr[i];
+	}
+	void selectionSort(int arr[], int n)
+	{
+	    for(int i=0,imin;i<n-1;i++){
+	        imin=i;
+	        for(int j=i+1;j<n;j++){
+	            if(arr[imin]>arr[j])imin=j;
+	        }
+	        int t=arr[i];
+	        arr[i]=arr[imin];
+	        arr[imin]=t;
+	        select(arr,i);
+	    }
+	}
+}
