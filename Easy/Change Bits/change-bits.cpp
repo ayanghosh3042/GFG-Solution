8e79ebd2c64e2d6454c91961903ef907777117de
Ayan Ghosh <146657282+ayanghosh3042@users.noreply.github.com>
2024-05-31 12:08:55 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Change Bits/change-bits.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    vector<int> changeBits(int N) {
+        int n=log2(N)+1;
+        int m=(1<<n)-1;
+        return {m-N,m};
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        Solution ob;
+        auto ans = ob.changeBits(N);
+        cout<<ans[0]<<" "<<ans[1]<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
