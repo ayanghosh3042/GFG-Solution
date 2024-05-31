025389a8d98a3235ecc06371d14aa9593a485c12
Ayan Ghosh <146657282+ayanghosh3042@users.noreply.github.com>
2024-05-31 12:40:58 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/The Bit Game/the-bit-game.cpp
@@ -0,0 +1,32 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+class Solution{   
+public:
+    int swapBitGame(long long N){
+        int i=0;
+        while(N){
+            i+=N%2;
+            N/=2;
+        }
+        return (i%2)?1:2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long N;
+        cin >> N;
+        Solution ob;
+        cout << ob.swapBitGame(N) << endl;
+    }
+    return 0; 
+} 
+// } Driver Code Ends
