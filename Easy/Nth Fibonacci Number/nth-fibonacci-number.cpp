4d730038ef8446e16ee9f83d02023501b8eaf372
Ayan Ghosh <146657282+ayanghosh3042@users.noreply.github.com>
2024-06-09 14:20:14 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -0,0 +1,36 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    int nthFibonacci(int n){
+        int x=1;
+        int y=1;
+        int mod=1e9+7;
+        for(int i=3;i<=n;i++){
+            int t=x;
+            x=y;
+            y=(t+y)%mod;
+        }
+        return y;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution ob;
+        cout << ob.nthFibonacci(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
