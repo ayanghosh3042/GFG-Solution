42f472195688722f06f57431e28413025832ab58
Ayan Ghosh <146657282+ayanghosh3042@users.noreply.github.com>
2024-06-22 10:55:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Maximum Nesting Depth of the Parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int maxDepth(string s) {
+        int count=0,max=0;
+        for( auto i : s){
+            if(i=='(')count++;
+            else if(i==')')count--;
+            max=(count>max)?count:max;
+        }
+        return max;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+
+        Solution obj;
+        int ans = obj.maxDepth(s);
+        cout << ans << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
